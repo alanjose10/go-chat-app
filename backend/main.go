815a6b59794a179cbc07a2d9f329bd6633d52d8c
Alan Jose <alanjose10@gmail.com>
2024-06-16 21:39:53 +0100

    Creating ws connection and testing message sent from client

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,52 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	reader(ws)
+}
 
 func setupRoutes() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Simple Server")
 	})
+
+	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
---
+++ frontend/src/api/index.js
@@ -0,0 +1,29 @@
+var socket = new WebSocket("ws://localhost:8080/ws")
+
+let connect = () => {
+    console.log("Attempting connection...");
+
+    socket.onopen = () => {
+        console.log("successfully connected")
+    };
+
+    socket.onmessage = msg => {
+        console.log(msg);
+    };
+
+    socket.onclose = event => {
+        console.log("Socket closed connection: ", event);
+    };
+
+    socket.onerror = error => {
+        console.log("Socket error: ", error);
+    };
+
+};
+
+var sendMsg = msg => {
+    console.log("sending msg: ", msg);
+    socket.send(msg);
+};
+
+export { connect, sendMsg };
