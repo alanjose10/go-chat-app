43d8a543fdebee2d3b4b22f1d2fd9795908d050b
Alan Jose <alanjose10@gmail.com>
2024-06-16 14:58:32 +0100

    even or odd

--- cards/main.go
+++ cards/main.go
@@ -2,11 +2,26 @@ package main
 
 func main() {
 
-	// fileName := "my_cards"
+	fileName := "my_cards"
 
 	cards := newDeck()
 
 	cards.shuffleDeck()
 	cards.printDeck()
 
+	cards.saveToFile(fileName)
+
+	// sl := []int{}
+
+	// for i := range 10 {
+	// 	sl = append(sl, i)
+	// }
+
+	// for _, v := range sl {
+	// 	if v%2 == 1 {
+	// 		fmt.Println("odd")
+	// 	} else {
+	// 		fmt.Println("even")
+	// 	}
+	// }
 }
