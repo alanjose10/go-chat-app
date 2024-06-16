6375c5009cb92a4776174d8fe7b1c003907648b7
Alan Jose <alanjose10@gmail.com>
2024-06-16 15:27:27 +0100

    create simple websocket server

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+}
 
 func main() {
 	fmt.Println("Chat App v0.1")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
